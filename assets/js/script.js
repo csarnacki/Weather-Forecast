a5ec6ea1b68d54e679fa8a5ba95650aea40423c0
Chris <sirchrissarnacki@gmail.com>
2023-03-05 16:52:04 -0800

    Created JS comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,60 +7,30 @@ console.log(date.format());
 let time = dayjs();
 console.log(time.format('YYYY-MM-DD HH:MM:SS'));
 
-var cityHist = [];
-//Will save the text value of the search and save it to an array and storage
+let cityHistory = [];
+//Saves value of search to local storage
 $('.search').on("click", function (event) {
 	event.preventDefault();
 	city = $(this).parent('.btnPar').siblings('.textVal').val().trim();
 	if (city === "") {
 		return;
 	};
-	cityHist.push(city);
+	cityHistory.push(city);
 
-	localStorage.setItem('city', JSON.stringify(cityHist));
-	fiveForecastEl.empty();
-	getHistory();
-	getWeatherToday();
+	localStorage.setItem('city', JSON.stringify(cityHistory));
+	weatherForecast.empty();
+	weatherToday();
 });
 
-//Will create buttons based on search history 
-var contHistEl = $('.cityHist');
-function getHistory() {
-	contHistEl.empty();
-
-	for (let i = 0; i < cityHist.length; i++) {
-
-		var rowEl = $('<row>');
-		var btnEl = $('<button>').text(`${cityHist[i]}`)
-
-		rowEl.addClass('row histBtnRow');
-		btnEl.addClass('btn btn-outline-secondary histBtn');
-		btnEl.attr('type', 'button');
-
-		contHistEl.prepend(rowEl);
-		rowEl.append(btnEl);
-	} if (!city) {
-		return;
-	}
-	//Allows the buttons to start a search as well
-	$('.histBtn').on("click", function (event) {
-		event.preventDefault();
-		city = $(this).text();
-		fiveForecastEl.empty();
-		getWeatherToday();
-	});
-};
-
-//Grab the main 'Today' card body.
-var cardTodayBody = $('.cardBodyToday')
-//Applies the weather data to the today card and then launches the five day forecast
-function getWeatherToday() {
-	var getUrlCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}`;
+let cardTodayBody = $('.cardBodyToday')
+//Applies weather info to current day card and calls forecast function
+function weatherToday() {
+	let getURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}`;
 
 	$(cardTodayBody).empty();
 
 	$.ajax({
-		url: getUrlCurrent,
+		url: getURL,
 		method: 'GET',
 	}).then(function (response) {
 		$('.cardTodayCityName').text(response.name);
@@ -68,30 +38,30 @@ function getWeatherToday() {
 		//Icons
 		$('.icons').attr('src', `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`);
 		// Temperature
-		var pEl = $('<p>').text(`Temperature: ${response.main.temp} °F`);
-		cardTodayBody.append(pEl);
+		let temp = $('<p>').text(`Temperature: ${response.main.temp} °F`);
+		cardTodayBody.append(temp);
 		//Humidity
-		var pElHumid = $('<p>').text(`Humidity: ${response.main.humidity} %`);
-		cardTodayBody.append(pElHumid);
+		let humid = $('<p>').text(`Humidity: ${response.main.humidity} %`);
+		cardTodayBody.append(humid);
 		//Wind Speed
-		var pElWind = $('<p>').text(`Wind Speed: ${response.wind.speed} MPH`);
-		cardTodayBody.append(pElWind);
+		let wind = $('<p>').text(`Wind Speed: ${response.wind.speed} MPH`);
+		cardTodayBody.append(wind);
 	});
-	getFiveDayForecast();
+	fiveDayForecast();
 };
 
-var fiveForecastEl = $('.fiveForecast');
+let weatherForecast = $('.fiveForecast');
 
-function getFiveDayForecast() {
-	var getUrlFiveDay = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${key}`;
+function fiveDayForecast() {
+	let forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${key}`;
 
 	$.ajax({
-		url: getUrlFiveDay,
+		url: forecastURL,
 		method: 'GET',
 	}).then(function (response) {
-		var fiveDayArray = response.list;
-		var myWeather = [];
-		//Made a object that would allow for easier data read
+		let fiveDayArray = response.list;
+		let myWeather = [];
+		
 		$.each(fiveDayArray, function (index, value) {
 			testObj = {
 				date: value.dt_txt.split(' ')[0],
@@ -105,36 +75,36 @@ function getFiveDayForecast() {
 				myWeather.push(testObj);
 			}
 		})
-		//Inject the cards to the screen 
+		//Adding the cards to the screen 
 		for (let i = 0; i < myWeather.length; i++) {
 
-			var divElCard = $('<div>');
-			divElCard.attr('class', 'card text-white bg-primary mb-3 cardOne');
-			divElCard.attr('style', 'max-width: 200px;');
-			fiveForecastEl.append(divElCard);
+			let divCard = $('<div>');
+			divCard.attr('class', 'card text-white bg-primary mb-3 cardOne');
+			divCard.attr('style', 'max-width: 200px;');
+			weatherForecast.append(divCard);
 
-			var divElHeader = $('<div>');
-			divElHeader.attr('class', 'card-header')
-			var m = moment(`${myWeather[i].date}`).format('MM-DD-YYYY');
-			divElHeader.text(m);
-			divElCard.append(divElHeader)
+			let divHeader = $('<div>');
+			divHeader.attr('class', 'card-header')
+			let moment = dayjs(`${myWeather[i].date}`).format('MM-DD-YYYY');
+			divHeader.text(moment);
+			divCard.append(divHeader)
 
-			var divElBody = $('<div>');
-			divElBody.attr('class', 'card-body');
-			divElCard.append(divElBody);
+			let divBody = $('<div>');
+			divBody.attr('class', 'card-body');
+			divCard.append(divBody);
 
-			var divElIcon = $('<img>');
-			divElIcon.attr('class', 'icons');
-			divElIcon.attr('src', `https://openweathermap.org/img/wn/${myWeather[i].icon}@2x.png`);
-			divElBody.append(divElIcon);
+			let divIcon = $('<img>');
+			divIcon.attr('class', 'icons');
+			divIcon.attr('src', `https://openweathermap.org/img/wn/${myWeather[i].icon}@2x.png`);
+			divBody.append(divIcon);
 
 			//Temp
-			var pElTemp = $('<p>').text(`Temperature: ${myWeather[i].temp} °F`);
-			divElBody.append(pElTemp);
+			let temp = $('<p>').text(`Temperature: ${myWeather[i].temp} °F`);
+			divBody.append(temp);
 		
 			//Humidity
-			var pElHumid = $('<p>').text(`Humidity: ${myWeather[i].humidity} %`);
-			divElBody.append(pElHumid);
+			let humid = $('<p>').text(`Humidity: ${myWeather[i].humidity} %`);
+			divBody.append(humid);
 		}
 	});
 };
